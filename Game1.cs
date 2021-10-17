b4a76c8657a3d605d92570ef1ef685295b67d5a9
awaba <awaba@DELL-01>
2021-10-17 17:40:48 +0100

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Random_Track_Generation
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D greenRectangle;
+        Texture2D dot;
+
+        Vector2 gameBorderTL = new Vector2(320, 0); //Top Left Corner of game section
+        Vector2 gameBorderBR = new Vector2(1920, 1080); //Bottom Right Corner of game section
+
+        TrackGenerator Generator;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            
+            _graphics.PreferredBackBufferWidth = 1920;
+            _graphics.PreferredBackBufferHeight = 1080;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //Make the Green Rectangle for the background
+            greenRectangle = loadRectangle(Convert.ToInt32(_graphics.PreferredBackBufferWidth - gameBorderTL.X), _graphics.PreferredBackBufferHeight, Color.Green);
+
+            //Make a small red square as temporary markers for the points
+            dot = loadRectangle(10, 10, Color.Red);
+
+            Generator = new TrackGenerator(gameBorderTL, gameBorderBR, dot);
+
+        }
+
+        Texture2D loadRectangle(int width, int height, Color color)
+        {
+            Texture2D texture = new Texture2D(GraphicsDevice, width, height);
+            Color[] colourPixels = new Color[width * height];
+            for (int i = 0; i < colourPixels.Length; i++)
+            {
+                colourPixels[i] = color;
+            }
+            texture.SetData<Color>(colourPixels);
+
+            return texture;
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.WhiteSmoke);
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(greenRectangle, gameBorderTL, Color.White);
+
+            Generator.Draw(_spriteBatch);
+
+
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TrackGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Random_Track_Generation
+{
+    class TrackGenerator
+    {
+        Random rand = new Random();
+        int numberOfPoints;
+        Vector2[] trackPoints;
+        Texture2D dot;
+
+        public TrackGenerator(Vector2 gameBorderTL, Vector2 gameBorderBR, Texture2D texture)
+        {
+            dot = texture;
+
+            InitialisePoints(gameBorderTL, gameBorderBR);
+        }
+
+        void InitialisePoints(Vector2 gameBorderTL, Vector2 gameBorderBR)
+        {
+            //Decide how many points/turns you want in the track - decided randomly
+            numberOfPoints = rand.Next(3, 16);
+            trackPoints = new Vector2[numberOfPoints];
+
+            //generate the random points
+            for (int i = 0; i < trackPoints.Length; i++)
+            {
+                trackPoints[i].X = rand.Next(Convert.ToInt32(gameBorderTL.X + 20), Convert.ToInt32(gameBorderBR.X - 20));
+                trackPoints[i].Y = rand.Next(Convert.ToInt32(gameBorderTL.Y + 20), Convert.ToInt32(gameBorderBR.Y - 20));
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < trackPoints.Length; i++)
+            {
+                spriteBatch.Draw(dot, trackPoints[i], Color.Red);
+            }
+        }
+
+
+    }
+}
